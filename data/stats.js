4a583f7ab7550761286bc2bae1d5ea0d89d23170
X <hktang@users.noreply.github.com>
2022-11-04 22:25:18 +0900

    Add period at the end of sentence.

--- data/stats.js
+++ data/stats.js
@@ -18,13 +18,13 @@ export const stats = [
         pre: 'These activities have produced',
         number: '100+',
         unit: 'outputs',
-        post: 'from 14 loss and damage-related projects'
+        post: 'from 14 loss and damage-related projects.'
     },
     {
         key: 3,
         pre: 'Among the outputs',
         number: '8',
         unit: 'papers',
-        post: 'have been cited in the IPCC AR6'
+        post: 'have been cited in the IPCC AR6.'
     }
 ];
